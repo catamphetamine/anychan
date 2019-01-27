09ab7ded965a38d5034b62bc424d641407b867e0
catamphetamine <purecatamphetamine@gmail.com>
2019-01-27 06:31:23 +0300

    Footer margin and footnote
    Former-commit-id: 330d5a08f0a237f79355cbf20cf74e9b4c4e17e2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,16 @@ import './Footer.css'
 export default function Footer() {
 	return (
 		<footer className="webpage__footer">
+			<div className="webpage__footer-copyright">
+				<a
+					target="_blank"
+					href="https://github.com/catamphetamine/chanchan"
+					className="webpage__footer-link">
+					chanchan imageboard browser
+				</a>
+				{' '}
+				ver. 0.0.1
+			</div>
 			<ApplicationMenu/>
 		</footer>
 	)
