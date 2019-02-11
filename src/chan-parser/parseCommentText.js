fd1f77de8518c775779a90071b59be9efb52b3ea
catamphetamine <purecatamphetamine@gmail.com>
2019-02-11 06:38:09 +0300

    HTML parser fix markup
    Former-commit-id: 703191e06bcf814d6218278a4a97d6585e796679

--- src/chan-parser/parseCommentText.js
+++ src/chan-parser/parseCommentText.js
@@ -27,14 +27,31 @@ class CommentTextParser {
 		// )
 		// Parse into paragraphs.
 		if (this.options.parseParagraphs !== false) {
-			const paragraphs = comment.split(/<br>\s*(?:\s*<br>)+/)
-			return paragraphs.filter(_ => _).map(_ => _.trim()).map(this.parseParagraph)
+			const paragraphs = comment.split(/<br>(?:\s*<br>)+/)
+			return paragraphs
+				.filter(_ => _)
+				.map(_ => _.trim())
+				// There are cases when people write:
+				// "<strong>Abc<br><br>Def</strong>" on 2ch.hk.
+				// In such cases it will split the HTML into two paragraphs:
+				// "<strong>Abc" and "Def</strong>".
+				// I consider such cases penalized because it's an abuse:
+				// message syntax should stay as simple and conventional as possible.
+				// Therefore such "invalid" unpaired HTML tags
+				// are simply stripped from paragraph HTML.
+				.map(_ => _.trim())
+				.map(this.parseParagraph)
 		}
 		// Remove excessive `<br>`s.
 		comment = comment
-			.replace(/<br>(<br>)+/g, '<br><br>')
+			// Convert more than two `<br>`s into two `<br>`s.
+			.replace(/<br>(\s*<br>)+/g, '<br><br>')
+			// Trim one or two `<br>`s in the beginning.
 			.replace(/^<br>(<br>)?/, '')
+			// Trim one or two `<br>`s in the end.
 			.replace(/<br>(<br>)?$/, '')
+			// Trim whitespace.
+			.trim()
 		return [this.parseParagraph(comment)]
 	}
 
@@ -45,84 +62,126 @@ class CommentTextParser {
 		// Normalize `<br>`s so that they don't break parsing (`findClosingTagPosition()`).
 		text = text.replace(/\s*<br>/g, '<br/>')
 		// text = removeInvalidClosingTags(text)
-		for (const plugin of this.options.parseCommentTextPlugins) {
-			const parsed = this.parseHtmlTag(text, plugin)
-			if (parsed) {
-				return parsed.filter(_ => _)
-				// if (Array.isArray(parsed)) {
-				// 	return parsed.filter(_ => _)
-				// }
-				// // Invalid HTML markup was corrected.
-				// return this.parseParagraph(parsed)
+		const nextTag = this.findNextTag(text)
+		if (nextTag) {
+			if (typeof nextTag === 'string') {
+				// HTML corrected.
+				text = nextTag
+			} else {
+				for (const plugin of this.options.parseCommentTextPlugins) {
+					const parsed = this.parseHtmlTag(text, nextTag, plugin)
+					if (parsed) {
+						return parsed.filter(_ => _)
+					}
+				}
 			}
 		}
 		// Generic text.
 		return [this.correctGrammar(unescapeContent(text))]
 	}
 
-	parseHtmlTag(text, { opener, attributes, canContainChildren, correctContent, createBlock }) {
+	findNextTag(text) {
 		const firstTagStartsAt = text.indexOf('<')
 		if (firstTagStartsAt < 0) {
 			return
 		}
-		// First parse outer tags, then inner tags.
-		if (text.indexOf('<' + opener) !== firstTagStartsAt) {
+		const firstTagEndsAt = text.indexOf('>', firstTagStartsAt + '<'.length)
+		// Check for invalid HTML syntax.
+		if (firstTagEndsAt < 0) {
 			return
 		}
-		const startsAt = text.indexOf('<' + opener)
-		if (startsAt >= 0) {
-			const openerEndsAt = text.indexOf('>', startsAt + '<'.length)
-			// Parse content.
-			let endsAt
-			let content
-			if (canContainChildren !== false) {
-				const contentStartsAt = text.indexOf('>', startsAt + '<'.length) + '>'.length
-				const contentEndsAt = findClosingTagPosition(text, contentStartsAt)
-				// Invalid HTML markup.
-				// Ignore.
-				if (!contentEndsAt) {
-					console.error('Invalid HTML markup', text)
-					return
-				}
-				endsAt = text.indexOf('>', contentEndsAt) + '>'.length
-				content = text.slice(contentStartsAt, contentEndsAt)
-				// If there are nested tags then maybe parse them.
-				// Post links seem to contain unescaped `>>`s which is a bug.
-				if (content.indexOf('<') >= 0) {
-					content = this.parseParagraph(content)
-				} else {
-					content = correctContent === false ? unescapeContent(content) : this.correctGrammar(unescapeContent(content))
-				}
-			} else {
-				endsAt = text.indexOf('>', startsAt) + '>'.length
-			}
-			// Parse attributes.
-			const _attributes = []
-			if (attributes) {
-				const markup = text.slice(startsAt, openerEndsAt + 1)
-				for (const attribute of attributes) {
-					const attributeDefinitionStartsAt = markup.indexOf(`${attribute}="`)
-					if (attributeDefinitionStartsAt < 0) {
-						_attributes.push()
-						continue
-					}
-					const attributeStartsAt = attributeDefinitionStartsAt + `${attribute}="`.length
-					const attributeEndsAt = markup.indexOf('"', attributeStartsAt)
-					_attributes.push(
-						unescapeContent(
-							markup.slice(attributeStartsAt, attributeEndsAt)
-						)
-					)
-				}
-			}
-			// Using `.concat` to skip `undefined`s.
-			// Alternatively could use `.filter()`.
-			return [].concat(
-				this.parseParagraph(text.slice(0, startsAt)),
-				createBlock(content, _attributes),
-				this.parseParagraph(text.slice(endsAt))
-			)
+		// Check for invalid HTML syntax.
+		if (text[firstTagStartsAt + '<'.length] === '/') {
+			// Invalid HTML markup: non-matched closing tag.
+			console.error(`Invalid HTML markup: non-matched closing tag found\n\n${text}`)
+			// Correct HTML markup: remove the closing tag.
+			return text.slice(0, firstTagStartsAt) + text.slice(firstTagEndsAt + '>'.length)
 		}
+		// Get tag content.
+		const openerContent = text.slice(firstTagStartsAt + '<'.length, firstTagEndsAt)
+		// Get tag name.
+		const tagName = openerContent.indexOf(' ') > 0 ? openerContent.slice(0, openerContent.indexOf(' ')) : openerContent
+		return {
+			tagName,
+			openerContent,
+			openerStartsAt: firstTagStartsAt,
+			openerEndsAt: firstTagEndsAt
+		}
+	}
+
+	parseHtmlTag(text, { tagName, openerContent, openerStartsAt, openerEndsAt }, { tag, opener, attributes, canContainChildren, correctContent, createBlock }) {
+		if (!opener) {
+			opener = tagName
+		}
+		// First parse outer tags, then inner tags.
+		if (text.indexOf('<' + opener) !== openerStartsAt) {
+			return
+		}
+		// Parse content.
+		const startsAt = openerStartsAt
+		let endsAt
+		let content
+		if (canContainChildren !== false) {
+			const contentStartsAt = openerEndsAt + '>'.length
+			const contentEndsAt = findClosingTagPosition(text, contentStartsAt)
+			if (contentEndsAt < 0) {
+				// Invalid HTML markup: no closing tag found.
+				console.error(`Invalid HTML markup: no closing tag found for "${tagName}"\n\n${text}`)
+				// Correct HTML markup: remove the opening tag.
+				return this.parseParagraph(text.slice(0, startsAt) + text.slice(contentStartsAt))
+			}
+			endsAt = text.indexOf('>', contentEndsAt) + '>'.length
+			// Check for invalid HTML syntax.
+			if (endsAt < 0) {
+				// Invalid HTML markup: no closing tag found.
+				console.error(`Invalid HTML markup: no closing tag found for "${tagName}"\n\n${text}`)
+				// Correct HTML markup: remove the opening tag.
+				return this.parseParagraph(text.slice(0, startsAt) + text.slice(contentStartsAt))
+			}
+			const isSameClosingTag = isStringAt(tagName, text, contentEndsAt + '</'.length)
+			if (!isSameClosingTag) {
+				// Invalid HTML markup: non-matching closing tag.
+				console.error(`Invalid HTML markup: non-matching closing tag for tag "${tagName}"\n\n${text}`)
+				// Correct HTML markup: remove the closing tag.
+				return this.parseParagraph(text.slice(0, contentEndsAt) + text.slice(endsAt + '>'.length))
+			}
+			content = text.slice(contentStartsAt, contentEndsAt)
+			// If there are nested tags then maybe parse them.
+			// Post links seem to contain unescaped `>>`s which is a bug.
+			if (content.indexOf('<') >= 0) {
+				content = this.parseParagraph(content)
+			} else {
+				content = correctContent === false ? unescapeContent(content) : this.correctGrammar(unescapeContent(content))
+			}
+		} else {
+			endsAt = text.indexOf('>', startsAt) + '>'.length
+		}
+		// Parse attributes.
+		const _attributes = []
+		if (attributes) {
+			const markup = openerContent
+			for (const attribute of attributes) {
+				const attributeDefinitionStartsAt = markup.indexOf(`${attribute}="`)
+				if (attributeDefinitionStartsAt < 0) {
+					_attributes.push()
+					continue
+				}
+				const attributeStartsAt = attributeDefinitionStartsAt + `${attribute}="`.length
+				const attributeEndsAt = markup.indexOf('"', attributeStartsAt)
+				_attributes.push(
+					unescapeContent(
+						markup.slice(attributeStartsAt, attributeEndsAt)
+					)
+				)
+			}
+		}
+		// Using `.concat` to skip `undefined`s.
+		// Alternatively could use `.filter()`.
+		return [].concat(
+			this.parseParagraph(text.slice(0, startsAt)),
+			createBlock(content, _attributes),
+			this.parseParagraph(text.slice(endsAt))
+		)
 	}
 
 	correctGrammar(text) {
@@ -133,11 +192,16 @@ class CommentTextParser {
 	}
 }
 
-// function removeInvalidClosingTags(text) {
-// 	const firstTagStartsAt = text.indexOf('<')
-// 	if (text[firstTagStartsAt + 1] === '/') {
-// 		const firstTagEndsAt = text.indexOf('>', firstTagStartsAt)
-// 		return removeInvalidClosingTags(text.slice(firstTagEndsAt >= 0 ? firstTagEndsAt + 1 : firstTagStartsAt + 1))
-// 	}
-// 	return text
-// }
+// Won't work for "exotic" UTF-8 characters
+// (the ones consisting of four bytes)
+// but HTML tag characters don't fall into that range.
+function isStringAt(substring, string, index) {
+	let i = 0
+	while (i < substring.length) {
+		if (substring[i] !== string[index + i]) {
+			return false
+		}
+		i++
+	}
+	return true
+}
