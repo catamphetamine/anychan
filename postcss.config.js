9e1838e9803010b008200b2669345ca61cc23f7d
catamphetamine <purecatamphetamine@gmail.com>
2019-02-24 18:44:08 +0300

    postcss config comments
    Former-commit-id: fd27c1554f428adf040e644c60b10ddbd1737e8c
    Former-commit-id: e2b555067b27105f0ab04476765907423805b819

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = require('../webapp-frontend/postcss.config')
 
-// `root` option doesn't seem to work.
+// `@import "react-responsive-ui/style.css";` in
+// `webapp-frontend/src/styles/react-responsive-ui.css`
+// throws "module not found" error.
+// This is a workaround for that.
+// (`postcss-import` `root` option didn't seem to work).
 module.exports.plugins['postcss-import'].resolve = (id, basedir, importOptions)=> {
 	if (/^react-responsive-ui/.test(id)) {
 		const path = require('path')
