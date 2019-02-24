f0289a0ea735268cc14783133aa2ebb188c3f78a
catamphetamine <purecatamphetamine@gmail.com>
2019-02-24 18:45:29 +0300

    Spaces -> Tabs
    Former-commit-id: 2b84773dacfeeb66cdc991e9c1ac0acf79a119c3
    Former-commit-id: 7672ee28857a5c95a43f5a9e210e0c7ca85ce6ab

--- src/render.js
+++ src/render.js
@@ -3,18 +3,18 @@ import { render } from 'react-website'
 import settings from './react-website'
 
 export default async function() {
-  // Renders the webpage on the client side
-  const result = await render(settings)
-  // If there was an error during the initial rendering
-  // then `result` will be `undefined`.
-  if (result) {
-	  const { store, rerender } = result
-	  // Webpack "Hot Module Replacement"
-	  if (module.hot) {
-	    module.hot.accept('./react-website', () => {
-	      store.hotReload(settings.reducers)
-	      rerender()
-	    })
-	  }
+	// Renders the webpage on the client side
+	const result = await render(settings)
+	// If there was an error during the initial rendering
+	// then `result` will be `undefined`.
+	if (result) {
+		const { store, rerender } = result
+		// Webpack "Hot Module Replacement"
+		if (module.hot) {
+			module.hot.accept('./react-website', () => {
+				store.hotReload(settings.reducers)
+				rerender()
+			})
+		}
 	}
 }
