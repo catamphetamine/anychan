1de2806309c9cb678daad80034b156fc8bec799e
catamphetamine <purecatamphetamine@gmail.com>
2019-04-06 21:02:30 +0300

    Update bfg.sh
    Former-commit-id: 36c6be2001f646777c9f5b12417e99b20e012691

--- bfg.sh
+++ bfg.sh
@@ -1,1 +1,6 @@
+# Run Git garbage collector.
+git gc
+# Display repository size.
+git count-objects -vH
+# Run BFG repo cleaner.
 java -jar ../bfg-1.13.0.jar --strip-blobs-bigger-than 128K --protect-blobs-from master,gh-pages
