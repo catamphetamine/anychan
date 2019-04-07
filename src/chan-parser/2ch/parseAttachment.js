5c96b5a9e201850057ae2d76f1400c1d5755f3b1
catamphetamine <purecatamphetamine@gmail.com>
2019-04-07 15:07:42 +0300

    Refactored `parseAttachment`

--- src/chan-parser/2ch/parseAttachment.js
+++ src/chan-parser/2ch/parseAttachment.js
@@ -1,8 +1,113 @@
 import { getMimeTypeByFileName } from '../parseAttachment'
 
 const STICKER_FILE_TYPE = 100
-
 const ORIGIN = 'https://2ch.hk'
+
+export default function parseAttachment(file, { useRelativeUrls }) {
+	// `2ch` runs on multiple domains in case one of them is blocked.
+	// (2ch.hk, 2ch.so, 2ch.pm, 2ch.yt, 2ch.wf, 2ch.re).
+	// By using relative URLs in case of running on an "official" domain
+	// attachment URLs will keep working if `2ch.hk` domain is blocked.
+	const origin = useRelativeUrls ? '' : ORIGIN
+	const mimeType = getContentTypeByFileType(file.type) ||
+		// Fallback for incorrect attachments.
+		// (there were some cases supposedly in old threads)
+		getMimeTypeByFileName(file.path)
+	if (mimeType && mimeType.indexOf('image/') === 0) {
+		return parsePicture(file, mimeType, origin)
+	}
+	if (mimeType && mimeType.indexOf('video/') === 0) {
+		return parseVideo(file, mimeType, origin)
+	}
+	// `2ch.hk` doesn't allow attaching audio files
+	// but just in case such feature is added in some future.
+	if (mimeType && mimeType.indexOf('audio/') === 0) {
+		return parseAudio(file, mimeType, origin)
+	}
+	return parseFile(file, mimeType, origin)
+}
+
+function parsePicture(file, mimeType, origin) {
+	const attachment = {
+		type: 'picture',
+		picture: {
+			type: mimeType,
+			width: file.width,
+			height: file.height,
+			size: file.size * 1024, // in bytes
+			url: `${origin}${file.path}`,
+			sizes: [{
+				type: getMimeTypeByFileName(file.thumbnail),
+				width: file.tn_width,
+				height: file.tn_height,
+				url: `${origin}${file.thumbnail}`
+			}]
+		}
+	}
+	if (file.type === STICKER_FILE_TYPE) {
+		attachment.picture.transparent = true
+		// // A link to a page with "Add this sticker to your library" button.
+		// // Example: "/makaba/stickers/show/DJfQnwJM".
+		// ....installStickerLink = `${origin}${file.install}`
+	}
+	return attachment
+}
+
+function parseVideo(file, mimeType, origin) {
+	return {
+		type: 'video',
+		video: {
+			type: mimeType,
+			// Sometimes (very rarely) on `2ch.hk` `.webm`s have negative duration.
+			// Example:
+			// {
+			//   displayname: "6.4.webm",
+			//   duration: "0-2562047788:00:0-54",
+			//   duration_secs: -2077252342,
+			//   ...
+			// }
+			duration: file.duration_secs >= 0 ? file.duration_secs : 0,
+			width: file.width,
+			height: file.height,
+			size: file.size * 1024, // in bytes
+			url: `${origin}${file.path}`,
+			picture: {
+				type: getMimeTypeByFileName(file.thumbnail),
+				width: file.tn_width,
+				height: file.tn_height,
+				url: `${origin}${file.thumbnail}`
+			}
+		}
+	}
+}
+
+function parseAudio(file, mimeType, origin) {
+	const [name, ext] = splitFilename(file.fullname)
+	return {
+		type: 'audio',
+		audio: {
+			type: mimeType,
+			title: name,
+			url: `${origin}${file.path}`
+		}
+	}
+}
+
+function parseFile(file, mimeType, origin) {
+	const [name, ext] = splitFilename(file.fullname)
+	return {
+		type: 'file',
+		file: {
+			type: mimeType,
+			name,
+			ext,
+			size: file.size * 1024, // in bytes
+			width: file.width,
+			height: file.height,
+			url: `${origin}${file.path}`
+		}
+	}
+}
 
 function getContentTypeByFileType(type) {
 	switch (type) {
@@ -19,85 +124,6 @@ function getContentTypeByFileType(type) {
 		// Stickers.
 		case STICKER_FILE_TYPE:
 			return 'image/png'
-	}
-}
-
-export default function parseAttachment(file, { useRelativeUrls }) {
-	// `2ch` runs on multiple domains in case one of them is blocked.
-	// (2ch.hk, 2ch.so, 2ch.pm, 2ch.yt, 2ch.wf, 2ch.re).
-	// By using relative URLs in case of running on an "official" domain
-	// attachment URLs will keep working if `2ch.hk` domain is blocked.
-	const origin = useRelativeUrls ? '' : ORIGIN
-	let mimeType = getContentTypeByFileType(file.type)
-	// Fallback for incorrect attachments.
-	// (there were some cases supposedly in old threads)
-	if (!mimeType) {
-		mimeType = getMimeTypeByFileName(file.path)
-	}
-	if (mimeType && mimeType.indexOf('image/') === 0) {
-		const attachment = {
-			type: 'picture',
-			picture: {
-				type: mimeType,
-				width: file.width,
-				height: file.height,
-				size: file.size * 1024, // in bytes
-				url: `${origin}${file.path}`,
-				sizes: [{
-					type: getMimeTypeByFileName(file.thumbnail),
-					width: file.tn_width,
-					height: file.tn_height,
-					url: `${origin}${file.thumbnail}`
-				}]
-			}
-		}
-		if (file.type === STICKER_FILE_TYPE) {
-			attachment.picture.transparent = true
-			// // A link to a page with "Add this sticker to your library" button.
-			// // Example: "/makaba/stickers/show/DJfQnwJM".
-			// ....installStickerLink = `${origin}${file.install}`
-		}
-		return attachment
-	}
-	if (mimeType && mimeType.indexOf('video/') === 0) {
-		return {
-			type: 'video',
-			video: {
-				type: mimeType,
-				// Sometimes (very rarely) on `2ch.hk` `.webm`s have negative duration.
-				// Example:
-				// {
-				//   displayname: "6.4.webm",
-				//   duration: "0-2562047788:00:0-54",
-				//   duration_secs: -2077252342,
-				//   ...
-				// }
-				duration: file.duration_secs >= 0 ? file.duration_secs : 0,
-				width: file.width,
-				height: file.height,
-				size: file.size * 1024, // in bytes
-				url: `${origin}${file.path}`,
-				picture: {
-					type: getMimeTypeByFileName(file.thumbnail),
-					width: file.tn_width,
-					height: file.tn_height,
-					url: `${origin}${file.thumbnail}`
-				}
-			}
-		}
-	}
-	const [name, ext] = splitFilename(file.fullname)
-	return {
-		type: 'file',
-		file: {
-			type: mimeType,
-			name,
-			ext,
-			size: file.size * 1024, // in bytes
-			width: file.width,
-			height: file.height,
-			url: `${origin}${file.path}`
-		}
 	}
 }
 
--- src/chan-parser/4chan/parseAttachment.js
+++ src/chan-parser/4chan/parseAttachment.js
@@ -1,105 +1,126 @@
 import { getMimeTypeByFileName } from '../parseAttachment'
 
-function formatUrl(url, boardId, name, ext, originalName) {
-	return url
-		.replace(/{boardId}/g, boardId)
-		.replace(/{name}/g, name)
-		.replace(/{ext}/g, ext)
-		.replace(/{originalName}/g, originalName)
-}
-
-export default function parseAttachment(file, {
-	chan,
-	boardId,
-	attachmentUrl,
-	attachmentThumbnailUrl,
-	// `8ch.net` has `fpath: 0/1` parameter.
-	attachmentUrlFpath,
-	attachmentThumbnailUrlFpath,
-	fileAttachmentUrl
-}) {
+export default function parseAttachment(file, options) {
+	const { chan } = options
 	if (chan === '8ch') {
 		if (file.fpath) {
-			attachmentUrl = attachmentUrlFpath
-			attachmentThumbnailUrl = attachmentThumbnailUrlFpath
+			options = {
+				...options,
+				attachmentUrl: attachmentUrlFpath,
+				attachmentThumbnailUrl: attachmentThumbnailUrlFpath
+			}
 		}
 	}
 	const mimeType = getMimeTypeByFileName(file.ext)
 	if (mimeType && mimeType.indexOf('image/') === 0) {
-		const thumbnailExt = getThumbnailExt(file, 'picture', chan)
-		const thumbnailType = getMimeTypeByFileName(thumbnailExt)
-		const sizes = [{
-			type: thumbnailType,
-			width: file.tn_w,
-			height: file.tn_h,
-			url: formatUrl(attachmentThumbnailUrl, boardId, file.tim, thumbnailExt, file.filename)
-		}]
-		// `4chan.org` generates smaller copies of images (limited to 1024x1024)
-		// for images having both width and height greater than 1024px.
-		// These images are in the same location as usual but the filename ends with "m".
-		// `m_img` parameter indicates that this smaller image is available.
-		// https://github.com/4chan/4chan-API/issues/63
-		if (chan === '4chan' && file.m_img) {
-			const aspectRatio = file.w / file.h
-			sizes.push({
-				type: thumbnailType,
-				width: aspectRatio >= 1 ? 1024 : Math.round(1024 * aspectRatio),
-				height: aspectRatio >= 1 ? Math.round(1024 / aspectRatio) : 1024,
-				url: formatUrl(attachmentUrl, boardId, file.tim + 'm', thumbnailExt, file.filename)
-			})
-		}
-		const attachment = {
-			type: 'picture',
-			picture: {
-				type: mimeType,
-				width: file.w,
-				height: file.h,
-				size: file.fsize, // in bytes
-				url: formatUrl(attachmentUrl, boardId, file.tim, file.ext, file.filename),
-				sizes
-			}
-		}
-		// `8ch.net` and `4chan.org` have `spoiler: 0/1` on attachments.
-		if (file.spoiler) {
-			attachment.spoiler = true
-		}
-		return attachment
+		return parsePicture(file, mimeType, options)
 	}
 	if (mimeType && mimeType.indexOf('video/') === 0) {
-		const thumbnailExt = getThumbnailExt(file, 'video', chan)
-		const attachment = {
-			type: 'video',
-			video: {
-				type: mimeType,
-				width: file.w,
-				height: file.h,
-				size: file.fsize, // in bytes
-				url: formatUrl(attachmentUrl, boardId, file.tim, file.ext, file.filename),
-				picture: {
-					type: getMimeTypeByFileName(thumbnailExt),
-					width: file.tn_w,
-					height: file.tn_h,
-					url: formatUrl(attachmentThumbnailUrl, boardId, file.tim, thumbnailExt, file.filename)
-				}
-			}
-		}
-		// `8ch.net` and `4chan.org` have `spoiler: 0/1` on attachments.
-		if (file.spoiler) {
-			attachment.spoiler = true
-		}
-		return attachment
+		return parseVideo(file, mimeType, options)
 	}
 	// `kohlchan.net` supports attaching audio files.
 	if (mimeType && mimeType.indexOf('audio/') === 0) {
-		return {
-			type: 'audio',
-			audio: {
-				type: mimeType,
-				title: file.filename,
-				url: formatUrl(fileAttachmentUrl || attachmentUrl, boardId, file.tim, file.ext, file.filename)
+		return parseAudio(file, mimeType, options)
+	}
+	return parseFile(file, mimeType, options)
+}
+
+function parsePicture(file, mimeType, {
+	chan,
+	boardId,
+	attachmentUrl,
+	attachmentThumbnailUrl
+}) {
+	const thumbnailExt = getThumbnailExt(file, 'picture', chan)
+	const thumbnailType = getMimeTypeByFileName(thumbnailExt)
+	const sizes = [{
+		type: thumbnailType,
+		width: file.tn_w,
+		height: file.tn_h,
+		url: formatUrl(attachmentThumbnailUrl, boardId, file.tim, thumbnailExt, file.filename)
+	}]
+	// `4chan.org` generates smaller copies of images (limited to 1024x1024)
+	// for images having both width and height greater than 1024px.
+	// These images are in the same location as usual but the filename ends with "m".
+	// `m_img` parameter indicates that this smaller image is available.
+	// https://github.com/4chan/4chan-API/issues/63
+	if (chan === '4chan' && file.m_img) {
+		const aspectRatio = file.w / file.h
+		sizes.push({
+			type: thumbnailType,
+			width: aspectRatio >= 1 ? 1024 : Math.round(1024 * aspectRatio),
+			height: aspectRatio >= 1 ? Math.round(1024 / aspectRatio) : 1024,
+			url: formatUrl(attachmentUrl, boardId, file.tim + 'm', thumbnailExt, file.filename)
+		})
+	}
+	const attachment = {
+		type: 'picture',
+		picture: {
+			type: mimeType,
+			width: file.w,
+			height: file.h,
+			size: file.fsize, // in bytes
+			url: formatUrl(attachmentUrl, boardId, file.tim, file.ext, file.filename),
+			sizes
+		}
+	}
+	// `8ch.net` and `4chan.org` have `spoiler: 0/1` on attachments.
+	if (file.spoiler) {
+		attachment.spoiler = true
+	}
+	return attachment
+}
+
+function parseVideo(file, mimeType, {
+	chan,
+	boardId,
+	attachmentUrl,
+	attachmentThumbnailUrl
+}) {
+	const thumbnailExt = getThumbnailExt(file, 'video', chan)
+	const attachment = {
+		type: 'video',
+		video: {
+			type: mimeType,
+			width: file.w,
+			height: file.h,
+			size: file.fsize, // in bytes
+			url: formatUrl(attachmentUrl, boardId, file.tim, file.ext, file.filename),
+			picture: {
+				type: getMimeTypeByFileName(thumbnailExt),
+				width: file.tn_w,
+				height: file.tn_h,
+				url: formatUrl(attachmentThumbnailUrl, boardId, file.tim, thumbnailExt, file.filename)
 			}
 		}
 	}
+	// `8ch.net` and `4chan.org` have `spoiler: 0/1` on attachments.
+	if (file.spoiler) {
+		attachment.spoiler = true
+	}
+	return attachment
+}
+
+function parseAudio(file, mimeType, {
+	boardId,
+	fileAttachmentUrl,
+	attachmentUrl
+}) {
+	return {
+		type: 'audio',
+		audio: {
+			type: mimeType,
+			title: file.filename,
+			url: formatUrl(fileAttachmentUrl || attachmentUrl, boardId, file.tim, file.ext, file.filename)
+		}
+	}
+}
+
+function parseFile(file, mimeType, {
+	boardId,
+	attachmentUrl,
+	fileAttachmentUrl
+}) {
 	return {
 		type: 'file',
 		file: {
@@ -132,4 +153,12 @@ function getThumbnailExt(file, type, chan) {
 		return '.png'
 	}
 	return file.ext
+}
+
+function formatUrl(url, boardId, name, ext, originalName) {
+	return url
+		.replace(/{boardId}/g, boardId)
+		.replace(/{name}/g, name)
+		.replace(/{ext}/g, ext)
+		.replace(/{originalName}/g, originalName)
 }
