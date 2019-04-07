e7a1ba451ae8c7e015ecfd4f84765e790436d3cc
catamphetamine <purecatamphetamine@gmail.com>
2019-04-07 12:36:08 +0300

    MIME types

--- src/chan-parser/parseAttachment.js
+++ src/chan-parser/parseAttachment.js
@@ -1,16 +1,23 @@
 // https://www.iana.org/assignments/media-types/media-types.xhtml
 const MIME_TYPES = {
+	// Picture.
+	'svg': 'image/svg+xml',
 	'jpg': 'image/jpeg',
 	'jpeg': 'image/jpeg',
 	'gif': 'image/gif',
 	'png': 'image/png',
 	'bmp': 'image/bmp',
+	'webp': 'image/webp',
+	// Video.
 	'webm': 'video/webm',
 	'mp4': 'video/mp4',
+	'ogv': 'video/ogg',
+	// Audio.
 	'mp3': 'audio/mpeg',
 	'ogg': 'audio/ogg',
 	'flac': 'audio/flac',
 	'opus': 'audio/opus',
+	// Other.
 	'7z': 'application/x-7z-compressed',
 	'zip': 'application/zip',
 	'pdf': 'application/pdf',
