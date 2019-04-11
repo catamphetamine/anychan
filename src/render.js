413bfeb637a23574bc62744d001665d91fa48bb7
catamphetamine <purecatamphetamine@gmail.com>
2019-04-11 03:31:05 +0300

    Google Analytics

--- src/render.js
+++ src/render.js
@@ -10,6 +10,26 @@ export default async function() {
 		onNavigate(url, location, { dispatch, getState }) {
 			document.querySelector('main').focus()
 			dispatch(hideSidebar())
+			try {
+				// Set up Google Analytics via `gtag`.
+				gtag('config', 'UA-133449060-1', {
+					// Anonymize IP for all Google Analytics events.
+					// https://developers.google.com/analytics/devguides/collection/gtagjs/ip-anonymization
+					'anonymize_ip': true,
+					// Specifies what percentage of users should be tracked.
+					// This defaults to 100 (no users are sampled out) but
+					// large sites may need to use a lower sample rate
+					// to stay within Google Analytics processing limits.
+					// 'sample_rate': 1,
+					// Report "page view" event to Google Analytics.
+					// https://stackoverflow.com/questions/37655898/tracking-google-analytics-page-views-in-angular2
+					// https://developers.google.com/analytics/devguides/collection/gtagjs/single-page-applications
+					'page_path': location.pathname
+				})
+			} catch (error) {
+				// Testing Google Analytics.
+				console.error(error)
+			}
 		}
 	})
 	// If there was an error during the initial rendering
