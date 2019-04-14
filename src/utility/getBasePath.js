9c18750c072557ac6db82d7dcf39b7a80c4b9d33
catamphetamine <purecatamphetamine@gmail.com>
2019-04-14 15:23:21 +0300

    `basePath` config parameter

--- src/utility/getBasePath.js
+++ src/utility/getBasePath.js
@@ -1,5 +1,10 @@
-// `gh-pages` will have `/chanchan` base path.
+import configuration from '../configuration'
+
 export default function getBasePath() {
+	if (configuration.basePath) {
+		return configuration.basePath
+	}
+	// `gh-pages` will have `/chanchan` base path.
 	if (window.location.origin === 'https://catamphetamine.github.io') {
 		return window.location.pathname.slice(0, window.location.pathname.indexOf('/', '/'.length))
 	}
