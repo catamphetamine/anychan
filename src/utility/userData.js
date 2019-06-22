8b7f8cb1104ce42cfb1eb8c21ac4c927b1def9e0
catamphetamine <purecatamphetamine@gmail.com>
2019-06-22 19:59:52 +0300

    UserData

---
+++ src/utility/userData.js
@@ -0,0 +1,402 @@
+import {
+	getObject,
+	setObject,
+	deleteObject,
+	forEach as forEachLocalStorage
+} from 'webapp-frontend/src/utility/localStorage'
+
+export class UserData {
+	prefix = 'userData.'
+	data = {
+		favoriteBoards: {
+			type: 'list',
+			expires: false
+		},
+		hiddenAuthors: {
+			type: 'list',
+			expires: false
+		},
+		hiddenComments: {
+			type: 'boards-threads-comments'
+		},
+		hiddenThreads: {
+			type: 'boards-threads'
+		},
+		readComments: {
+			type: 'boards-threads-comment'
+		},
+		watchedThreads: {
+			type: 'boards-threads'
+		},
+		ownThreads: {
+			type: 'boards-threads'
+		},
+		ownComments: {
+			type: 'boards-threads-comments'
+		},
+		upvotedComments: {
+			type: 'boards-threads-comments'
+		},
+		downvotedComments: {
+			type: 'boards-threads-comments'
+		}
+	}
+
+	constructor(storage) {
+		this.storage = storage
+		// Create data access methods.
+		for (const key of Object.keys(this.data)) {
+			const data = this.data[key]
+			const [ addTo, removeFrom, getFrom ] = getFunctions(data.type)
+			this[`add${capitalize(key)}`] = (...args) => addTo.apply(this, [this.storage, this.prefix + key].concat(args))
+			this[`remove${capitalize(key)}`] = (...args) => removeFrom.apply(this, [this.storage, this.prefix + key].concat(args))
+			this[`get${capitalize(key)}`] = (...args) => getFrom.apply(this, [this.storage, this.prefix + key].concat(args))
+		}
+	}
+
+	onThreadExpire(boardId, threadId) {
+		for (const key of Object.keys(this.data)) {
+			const data = this.data[key]
+			switch (data.type) {
+				case 'boards-threads-comments':
+				case 'boards-threads-comment':
+				case 'boards-threads':
+					this[`remove${capitalize(key)}`](boardId, threadId)
+					break
+			}
+		}
+	}
+
+	clear = () => {
+		this.storage.forEach((key) => {
+			if (key.indexOf(PREFIX) === 0) {
+				this.storage.delete(key)
+			}
+		})
+	}
+}
+
+function capitalize(string) {
+	return string[0].toUpperCase() + string.slice(1)
+}
+
+function getFunctions(type) {
+	switch (type) {
+		// hiddenAuthors: [
+		//   'a0dbf7',
+		//   ...
+		// ]
+		// favoriteBoards: [
+		//   'a',
+		//   'b'
+		// ]
+		case 'list':
+			return [
+				addToList,
+				removeFromList,
+				getFromList
+			]
+		// hiddenThreads: {
+		//   a: [
+		//     123,
+		//     ...
+		//   ],
+		//   ...
+		// }
+		case 'boards-threads':
+			return [
+				addToBoardIdThreadIds,
+				removeFromBoardIdThreadIds,
+				getFromBoardIdThreadIds
+			]
+		// hiddenComments: {
+		//   a: {
+		//     '123': [
+		//       124,
+		//       125,
+		//       ...
+		//     ],
+		//     ...
+		//   },
+		//   ...
+		// }
+		case 'boards-threads-comments':
+			return [
+				addToBoardIdThreadIdCommentIds,
+				removeFromBoardIdThreadIdCommentIds,
+				getFromBoardIdThreadIdCommentIds
+			]
+		// readComments: {
+		//   a: {
+		//   	'124': 111, // Latest read comment id.
+		//   	'356': 333,
+		//   	...
+		//   ],
+		//   ...
+		// }
+		case 'boards-threads-comment':
+			return [
+				addToBoardIdThreadIdCommentId,
+				removeFromBoardIdThreadIdCommentId,
+				getFromBoardIdThreadIdCommentId
+			]
+	}
+}
+
+function addToBoardIdThreadIds(storage, key, boardId, threadId) {
+	const threadIdsByBoardId = storage.get(key, {})
+	if (!threadIdsByBoardId[boardId]) {
+		threadIdsByBoardId[boardId] = []
+	}
+	const index = threadIdsByBoardId[boardId].indexOf(threadId)
+	if (index < 0) {
+		threadIdsByBoardId[boardId].push(threadId)
+		storage.set(key, threadIdsByBoardId)
+	}
+}
+
+function removeFromBoardIdThreadIds(storage, key, boardId, threadId) {
+	const threadIdsByBoardId = storage.get(key)
+	if (!threadIdsByBoardId) {
+		return
+	}
+	if (!threadIdsByBoardId[boardId]) {
+		return
+	}
+	if (threadId) {
+		const index = threadIdsByBoardId[boardId].indexOf(threadId)
+		if (index < 0) {
+			return
+		}
+		threadIdsByBoardId[boardId].splice(index, 1)
+		if (threadIdsByBoardId[boardId].length === 0) {
+			delete threadIdsByBoardId[boardId]
+		}
+	} else {
+		delete threadIdsByBoardId[boardId]
+	}
+	if (Object.keys(threadIdsByBoardId).length === 0) {
+		storage.delete(key)
+	} else {
+		storage.set(key, threadIdsByBoardId)
+	}
+}
+
+function getFromBoardIdThreadIds(storage, key, boardId, threadId) {
+	const threadIdsByBoardId = storage.get(key, {})
+	if (boardId) {
+		const threadIds = threadIdsByBoardId[boardId] || []
+		if (threadId) {
+			const index = threadIds.indexOf(threadId)
+			return index >= 0
+		}
+		return threadIds
+	}
+	return threadIdsByBoardId
+}
+
+function addToBoardIdThreadIdCommentId(storage, key, boardId, threadId, commentId) {
+	const boardIdThreadIdCommentId = storage.get(key, {})
+	if (!boardIdThreadIdCommentId[boardId]) {
+		boardIdThreadIdCommentId[boardId] = {}
+	}
+	threadId = String(threadId)
+	boardIdThreadIdCommentId[boardId][threadId] = commentId
+	storage.set(key, boardIdThreadIdCommentId)
+}
+
+function removeFromBoardIdThreadIdCommentId(storage, key, boardId, threadId, commentId) {
+	const boardIdThreadIdCommentId = storage.get(key, {})
+	if (threadId) {
+		const threadIdCommentId = boardIdThreadIdCommentId[boardId]
+		if (!threadIdCommentId) {
+			return
+		}
+		threadId = String(threadId)
+		if (commentId) {
+			if (threadIdCommentId[threadId] !== commentId) {
+				return
+			}
+		}
+		delete threadIdCommentId[threadId]
+		if (Object.keys(threadIdCommentId).length === 0) {
+			delete boardIdThreadIdCommentId[boardId]
+		}
+	} else {
+		delete boardIdThreadIdCommentId[boardId]
+	}
+	if (Object.keys(boardIdThreadIdCommentId).length === 0) {
+		storage.delete(key)
+	} else {
+		storage.set(key, boardIdThreadIdCommentId)
+	}
+}
+
+function getFromBoardIdThreadIdCommentId(storage, key, boardId, threadId, commentId) {
+	const boardIdThreadIdCommentId = storage.get(key, {})
+	if (boardId) {
+		const threadIdCommentId = boardIdThreadIdCommentId[boardId] || {}
+		if (threadId) {
+			threadId = String(threadId)
+			const _commentId = threadIdCommentId[threadId]
+			if (commentId) {
+				return _commentId === commentId
+			}
+			return _commentId
+		}
+		return threadIdCommentId
+	}
+	return boardIdThreadIdCommentId
+}
+
+function addToBoardIdThreadIdCommentIds(storage, key, boardId, threadId, commentId) {
+	const boardIdThreadIdCommentIds = storage.get(key, {})
+	let threadIdCommentIds = boardIdThreadIdCommentIds[boardId]
+	if (!threadIdCommentIds) {
+		threadIdCommentIds = {}
+		boardIdThreadIdCommentIds[boardId] = threadIdCommentIds
+	}
+	threadId = String(threadId)
+	let commentIds = threadIdCommentIds[threadId]
+	if (!commentIds) {
+		commentIds = []
+		threadIdCommentIds[threadId] = commentIds
+	}
+	const index = commentIds.indexOf(commentId)
+	if (index < 0) {
+		commentIds.push(commentId)
+		storage.set(key, boardIdThreadIdCommentIds)
+	}
+}
+
+function removeFromBoardIdThreadIdCommentIds(storage, key, boardId, threadId, commentId) {
+	let boardIdThreadIdCommentIds = storage.get(key)
+	if (!boardIdThreadIdCommentIds) {
+		return
+	}
+	if (threadId) {
+		const threadIdCommentIds = boardIdThreadIdCommentIds[boardId]
+		if (!threadIdCommentIds) {
+			return
+		}
+		threadId = String(threadId)
+		if (commentId) {
+			const commentIds = threadIdCommentIds[threadId]
+			if (!commentIds) {
+				return
+			}
+			const index = commentIds.indexOf(commentId)
+			if (index < 0) {
+				return
+			}
+			commentIds.splice(index, 1)
+			if (commentIds.length === 0) {
+				delete threadIdCommentIds[threadId]
+			}
+		} else {
+			delete threadIdCommentIds[threadId]
+		}
+		if (Object.keys(threadIdCommentIds).length === 0) {
+			delete boardIdThreadIdCommentIds[boardId]
+		}
+	} else {
+		delete boardIdThreadIdCommentIds[boardId]
+	}
+	if (Object.keys(boardIdThreadIdCommentIds).length === 0) {
+		storage.delete(key)
+	} else {
+		storage.set(key, boardIdThreadIdCommentIds)
+	}
+}
+
+function getFromBoardIdThreadIdCommentIds(storage, key, boardId, threadId, commentId) {
+	const boardIdThreadIdCommentIds = storage.get(key, {})
+	if (boardId) {
+		const threadIdCommentIds = boardIdThreadIdCommentIds[boardId] || {}
+		if (threadId) {
+			threadId = String(threadId)
+			const commentIds = threadIdCommentIds[threadId] || []
+			if (commentId) {
+				const index = commentIds.indexOf(commentId)
+				return index >= 0
+			}
+			return commentIds
+		}
+		return threadIdCommentIds
+	}
+	return boardIdThreadIdCommentIds
+}
+
+function addToList(storage, key, item) {
+	const list = storage.get(key, [])
+	const index = list.indexOf(item)
+	if (index < 0) {
+		list.push(item)
+		storage.set(key, list)
+	}
+}
+
+function removeFromList(storage, key, item) {
+	const list = storage.get(key)
+	if (!list) {
+		return
+	}
+	const index = list.indexOf(item)
+	if (index < 0) {
+		return
+	}
+	list.splice(index, 1)
+	if (list.length === 0) {
+		storage.delete(key)
+	} else {
+		storage.set(key, list)
+	}
+}
+
+function getFromList(storage, key, item) {
+	const list = storage.get(key, [])
+	if (item) {
+		const index = list.indexOf(item)
+		return index >= 0
+	}
+	return list
+}
+
+export class LocalStorage {
+	get(key, defaultValue) {
+		return getObject(key, defaultValue)
+	}
+	set(key, value) {
+		return setObject(key, value)
+	}
+	delete(key) {
+		return deleteObject(key)
+	}
+	forEach(func) {
+		forEachLocalStorage(func)
+	}
+}
+
+export class MemoryStorage {
+	data = {}
+	get(key, defaultValue) {
+		return this.data[key] || defaultValue
+	}
+	set(key, value) {
+		this.data[key] = value
+	}
+	delete(key) {
+		delete this.data[key]
+	}
+	// `clear()` is only used for tests.
+	clear() {
+		this.data = {}
+	}
+	forEach(func) {
+		for (const key of Object.keys(this.data)) {
+			func(key)
+		}
+	}
+}
+
+export default new UserData(new LocalStorage())
---
+++ src/utility/userData.test.js
@@ -0,0 +1,578 @@
+import { describe, it } from 'webapp-frontend/src/utility/mocha'
+import expectToEqual from 'webapp-frontend/src/utility/expectToEqual'
+
+import { UserData, MemoryStorage } from './userData'
+
+const storage = new MemoryStorage()
+const userData = new UserData(storage)
+userData.prefix = ''
+
+describe('userData', () => {
+	it('should add/remove/get favorite boards', () => {
+		storage.clear()
+		userData.addFavoriteBoards('a')
+		expectToEqual(
+			storage.data,
+			{
+				favoriteBoards: [
+					'a'
+				]
+			}
+		)
+		userData.addFavoriteBoards('b')
+		expectToEqual(
+			storage.data,
+			{
+				favoriteBoards: [
+					'a',
+					'b'
+				]
+			}
+		)
+		expectToEqual(
+			userData.getFavoriteBoards(),
+			[
+				'a',
+				'b'
+			]
+		)
+		expectToEqual(
+			userData.getFavoriteBoards('a'),
+			true
+		)
+		expectToEqual(
+			userData.getFavoriteBoards('c'),
+			false
+		)
+		userData.removeFavoriteBoards('c')
+		expectToEqual(
+			storage.data,
+			{
+				favoriteBoards: [
+					'a',
+					'b'
+				]
+			}
+		)
+		userData.removeFavoriteBoards('b')
+		expectToEqual(
+			storage.data,
+			{
+				favoriteBoards: [
+					'a'
+				]
+			}
+		)
+		userData.removeFavoriteBoards('a')
+		expectToEqual(
+			storage.data,
+			{}
+		)
+	})
+
+	it('should add/remove/get watched threads', () => {
+		storage.clear()
+		userData.addWatchedThreads('a', 123)
+		expectToEqual(
+			storage.data,
+			{
+				watchedThreads: {
+					a: [
+						123
+					]
+				}
+			}
+		)
+		userData.addWatchedThreads('a', 456)
+		expectToEqual(
+			storage.data,
+			{
+				watchedThreads: {
+					a: [
+						123,
+						456
+					]
+				}
+			}
+		)
+		userData.addWatchedThreads('b', 789)
+		expectToEqual(
+			storage.data,
+			{
+				watchedThreads: {
+					a: [
+						123,
+						456
+					],
+					b: [
+						789
+					]
+				}
+			}
+		)
+		expectToEqual(
+			userData.getWatchedThreads(),
+			{
+				a: [
+					123,
+					456
+				],
+				b: [
+					789
+				]
+			}
+		)
+		expectToEqual(
+			userData.getWatchedThreads('a'),
+			[
+				123,
+				456
+			]
+		)
+		expectToEqual(
+			userData.getWatchedThreads('c'),
+			[]
+		)
+		expectToEqual(
+			userData.getWatchedThreads('a', 123),
+			true
+		)
+		expectToEqual(
+			userData.getWatchedThreads('a', 789),
+			false
+		)
+		expectToEqual(
+			userData.getWatchedThreads('c', 111),
+			false
+		)
+		userData.removeWatchedThreads('b')
+		expectToEqual(
+			storage.data,
+			{
+				watchedThreads: {
+					a: [
+						123,
+						456
+					]
+				}
+			}
+		)
+		userData.removeWatchedThreads('b', 789)
+		expectToEqual(
+			storage.data,
+			{
+				watchedThreads: {
+					a: [
+						123,
+						456
+					]
+				}
+			}
+		)
+		userData.removeWatchedThreads('a', 123)
+		expectToEqual(
+			storage.data,
+			{
+				watchedThreads: {
+					a: [
+						456
+					]
+				}
+			}
+		)
+		userData.removeWatchedThreads('a', 456)
+		expectToEqual(
+			storage.data,
+			{}
+		)
+	})
+
+	it('should add/remove/get read comments', () => {
+		storage.clear()
+		userData.addReadComments('a', 123, 124)
+		expectToEqual(
+			storage.data,
+			{
+				readComments: {
+					a: {
+						'123': 124
+					}
+				}
+			}
+		)
+		userData.addReadComments('a', 123, 125)
+		expectToEqual(
+			storage.data,
+			{
+				readComments: {
+					a: {
+						'123': 125
+					}
+				}
+			}
+		)
+		userData.addReadComments('a', 456, 456)
+		expectToEqual(
+			storage.data,
+			{
+				readComments: {
+					a: {
+						'123': 125,
+						'456': 456
+					}
+				}
+			}
+		)
+		userData.addReadComments('b', 789, 790)
+		expectToEqual(
+			storage.data,
+			{
+				readComments: {
+					a: {
+						'123': 125,
+						'456': 456
+					},
+					b: {
+						'789': 790
+					}
+				}
+			}
+		)
+		expectToEqual(
+			userData.getReadComments(),
+			{
+				a: {
+					'123': 125,
+					'456': 456
+				},
+				b: {
+					'789': 790
+				}
+			}
+		)
+		expectToEqual(
+			userData.getReadComments('b', 789, 790),
+			true
+		)
+		expectToEqual(
+			userData.getReadComments('b', 789, 791),
+			false
+		)
+		expectToEqual(
+			userData.getReadComments('b', 789),
+			790
+		)
+		expectToEqual(
+			userData.getReadComments('a'),
+			{
+				'123': 125,
+				'456': 456
+			}
+		)
+		expectToEqual(
+			userData.getReadComments('c'),
+			{}
+		)
+		expectToEqual(
+			userData.getReadComments('c', 111),
+			undefined
+		)
+		expectToEqual(
+			userData.getReadComments('c', 111, 112),
+			false
+		)
+		userData.removeReadComments('b')
+		expectToEqual(
+			storage.data,
+			{
+				readComments: {
+					a: {
+						'123': 125,
+						'456': 456
+					}
+				}
+			}
+		)
+		userData.removeReadComments('a', 456)
+		expectToEqual(
+			storage.data,
+			{
+				readComments: {
+					a: {
+						'123': 125
+					}
+				}
+			}
+		)
+		userData.removeReadComments('a', 123)
+		expectToEqual(
+			storage.data,
+			{}
+		)
+	})
+
+	it('should add/remove/get own comments', () => {
+		storage.clear()
+		userData.addOwnComments('a', 123, 124)
+		expectToEqual(
+			storage.data,
+			{
+				ownComments: {
+					a: {
+						'123': [
+							124
+						]
+					}
+				}
+			}
+		)
+		userData.addOwnComments('a', 123, 125)
+		expectToEqual(
+			storage.data,
+			{
+				ownComments: {
+					a: {
+						'123': [
+							124,
+							125
+						]
+					}
+				}
+			}
+		)
+		userData.addOwnComments('a', 456, 456)
+		expectToEqual(
+			storage.data,
+			{
+				ownComments: {
+					a: {
+						'123': [
+							124,
+							125
+						],
+						'456': [
+							456
+						]
+					}
+				}
+			}
+		)
+		userData.addOwnComments('b', 789, 790)
+		expectToEqual(
+			storage.data,
+			{
+				ownComments: {
+					a: {
+						'123': [
+							124,
+							125
+						],
+						'456': [
+							456
+						]
+					},
+					b: {
+						'789': [
+							790
+						]
+					}
+				}
+			}
+		)
+		expectToEqual(
+			userData.getOwnComments(),
+			{
+				a: {
+					'123': [
+						124,
+						125
+					],
+					'456': [
+						456
+					]
+				},
+				b: {
+					'789': [
+						790
+					]
+				}
+			}
+		)
+		expectToEqual(
+			userData.getOwnComments('b', 789, 790),
+			true
+		)
+		expectToEqual(
+			userData.getOwnComments('b', 789, 791),
+			false
+		)
+		expectToEqual(
+			userData.getOwnComments('b', 789),
+			[790]
+		)
+		expectToEqual(
+			userData.getOwnComments('a'),
+			{
+				'123': [
+					124,
+					125
+				],
+				'456': [
+					456
+				]
+			}
+		)
+		expectToEqual(
+			userData.getOwnComments('c'),
+			{}
+		)
+		expectToEqual(
+			userData.getOwnComments('c', 111),
+			[]
+		)
+		expectToEqual(
+			userData.getOwnComments('c', 111, 112),
+			false
+		)
+		userData.removeOwnComments('b')
+		expectToEqual(
+			storage.data,
+			{
+				ownComments: {
+					a: {
+						'123': [
+							124,
+							125
+						],
+						'456': [
+							456
+						]
+					}
+				}
+			}
+		)
+		userData.removeOwnComments('a', 456)
+		expectToEqual(
+			storage.data,
+			{
+				ownComments: {
+					a: {
+						'123': [
+							124,
+							125
+						]
+					}
+				}
+			}
+		)
+		userData.removeOwnComments('a', 123, 124)
+		expectToEqual(
+			storage.data,
+			{
+				ownComments: {
+					a: {
+						'123': [
+							125
+						]
+					}
+				}
+			}
+		)
+		userData.removeOwnComments('a', 123)
+		expectToEqual(
+			storage.data,
+			{}
+		)
+	})
+
+	it('should reset data on thread expiration', () => {
+		storage.clear()
+		userData.addWatchedThreads('a', 123)
+		userData.addWatchedThreads('a', 456)
+		userData.addWatchedThreads('b', 789)
+		userData.addReadComments('a', 123, 124)
+		userData.addReadComments('a', 456, 456)
+		userData.addReadComments('a', 456, 457)
+		userData.addReadComments('b', 789, 790)
+		userData.addOwnComments('a', 123, 124)
+		userData.addOwnComments('a', 123, 125)
+		userData.addOwnComments('a', 456, 456)
+		userData.addOwnComments('a', 456, 457)
+		userData.addOwnComments('b', 789, 790)
+		expectToEqual(
+			storage.data,
+			{
+				watchedThreads: {
+					a: [
+						123,
+						456
+					],
+					b: [
+						789
+					]
+				},
+				readComments: {
+					a: {
+						'123': 124,
+						'456': 457
+					},
+					b: {
+						'789': 790
+					}
+				},
+				ownComments: {
+					a: {
+						'123': [
+							124,
+							125
+						],
+						'456': [
+							456,
+							457
+						]
+					},
+					b: {
+						'789': [
+							790
+						]
+					}
+				}
+			}
+		)
+		userData.onThreadExpire('a', 123)
+		expectToEqual(
+			storage.data,
+			{
+				watchedThreads: {
+					a: [
+						456
+					],
+					b: [
+						789
+					]
+				},
+				readComments: {
+					a: {
+						'456': 457
+					},
+					b: {
+						'789': 790
+					}
+				},
+				ownComments: {
+					a: {
+						'456': [
+							456,
+							457
+						]
+					},
+					b: {
+						'789': [
+							790
+						]
+					}
+				}
+			}
+		)
+	})
+})
