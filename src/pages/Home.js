7adf6ee3d830a7bed881b7e65bc6477e5be19ca5
catamphetamine <purecatamphetamine@gmail.com>
2019-08-11 15:33:44 +0300

    "Show boards list" button fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 import PostBlock from 'webapp-frontend/src/components/PostBlock'
@@ -7,80 +8,88 @@ import ChanLogo, { hasLogo } from '../components/ChanLogo'
 
 import { getChan, getChanUrl } from '../chan'
 import getMessages from '../messages'
-import { showSidebar } from '../redux/app'
+import { showSidebar, setSidebarMode } from '../redux/app'
 
 import './Home.css'
 
 @connect(({ app }) => ({
 	locale: app.settings.locale
-}), {
-	showSidebar
-})
-export default class Home extends React.Component {
+}), dispatch => ({ dispatch }))
+export default class Home_ extends React.Component {
 	render() {
-		const {
-			locale,
-			showSidebar
-		} = this.props
+		return <Home {...this.props}/>
+	}
+}
 
-		const {
-			title,
-			subtitle,
-			description,
-			announcement,
-			links
-		} = getChan()
-
-		return (
-			<section className="home-page">
-				<div className="content text-content">
-					<div className="home-page__brand">
-						{hasLogo() &&
-							<a
-								target="_blank"
-								href={getChanUrl()}
-								className="home-page__logo-link">
-								<ChanLogo className="home-page__logo"/>
-							</a>
-						}
-						<div className="home-page__title">
-							<a
-								target="_blank"
-								href={getChanUrl()}>
-								{title}
-							</a>
-							<div className="home-page__subtitle">
-								{subtitle}
-							</div>
+function Home({
+	locale,
+	dispatch
+}) {
+	const {
+		title,
+		subtitle,
+		description,
+		announcement,
+		links
+	} = getChan()
+	const onShowBoardsList = useCallback(() => {
+		dispatch(showSidebar(true))
+		dispatch(setSidebarMode('boards'))
+	}, [dispatch])
+	return (
+		<section className="home-page">
+			<div className="content text-content">
+				<div className="home-page__brand">
+					{hasLogo() &&
+						<a
+							target="_blank"
+							href={getChanUrl()}
+							className="home-page__logo-link">
+							<ChanLogo className="home-page__logo"/>
+						</a>
+					}
+					<div className="home-page__title">
+						<a
+							target="_blank"
+							href={getChanUrl()}>
+							{title}
+						</a>
+						<div className="home-page__subtitle">
+							{subtitle}
 						</div>
 					</div>
-
-					{description &&
-						<div className="home-page__description">
-							<PostBlock>
-								{description}
-							</PostBlock>
-						</div>
-					}
-
-					{announcement &&
-						<div className="home-page__announcement">
-							<PostBlock>
-								{announcement}
-							</PostBlock>
-						</div>
-					}
-
-					<p className="home-page__show-boards-list">
-						<button
-							type="button"
-							onClick={showSidebar}
-							className="rrui__button-reset">
-							{getMessages(locale).boards.showBoardsList}
-						</button>
-					</p>
 				</div>
-			</section>
-		)
-	}
+
+				{description &&
+					<div className="home-page__description">
+						<PostBlock>
+							{description}
+						</PostBlock>
+					</div>
+				}
+
+				{announcement &&
+					<div className="home-page__announcement">
+						<PostBlock>
+							{announcement}
+						</PostBlock>
+					</div>
+				}
+
+				<p className="home-page__show-boards-list">
+					<button
+						type="button"
+						onClick={onShowBoardsList}
+						className="rrui__button-reset">
+						{getMessages(locale).boards.showBoardsList}
+					</button>
+				</p>
+			</div>
+		</section>
+	)
+}
+
+Home.propTypes = {
+	locale: PropTypes.string.isRequired,
+	dispatch: PropTypes.func.isRequired
 }
