4c59bf5f250ce15bb662d65a1b8120d077ad06bc
catamphetamine <purecatamphetamine@gmail.com>
2019-08-11 03:24:53 +0300

    .
    Former-commit-id: 3ba75d86c29cc318ac9ad7129d85655d07ddfeb5

---
+++ react-syntax-highlighter_languages_refractor_properties.e4079a3a15ebf87e0af9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[106],{1501:function(n,t,s){"use strict";function e(n){n.languages.properties={comment:/^[ \t]*[#!].*$/m,"attr-value":{pattern:/(^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?: *[=:] *| ))(?:\\(?:\r\n|[\s\S])|[^\\\r\n])+/m,lookbehind:!0},"attr-name":/^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?= *[=:] *| )/m,punctuation:/[=:]/}}n.exports=e,e.displayName="properties",e.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_properties.e4079a3a15ebf87e0af9.js.map
