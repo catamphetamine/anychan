536c30e058cb98ad785f5fbce1bf0b38fa1f5800
catamphetamine <purecatamphetamine@gmail.com>
2019-08-20 02:55:58 +0300

    .
    Former-commit-id: 09b12393c1aa566aaaa37fde34f2e13ebcb6428a

---
+++ react-syntax-highlighter_languages_refractor_arff.d1c6c10502821d1b5c40.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{1416:function(n,a,e){"use strict";function t(n){n.languages.arff={comment:/%.*/,string:{pattern:/(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,greedy:!0},keyword:/@(?:attribute|data|end|relation)\b/i,number:/\b\d+(?:\.\d+)?\b/,punctuation:/[{},]/}}n.exports=t,t.displayName="arff",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_arff.d1c6c10502821d1b5c40.js.map
