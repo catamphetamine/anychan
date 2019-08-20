c519feb1f4e13ecb336ff082cdf139af0b31a499
catamphetamine <purecatamphetamine@gmail.com>
2019-08-20 15:26:10 +0300

    `/captchan` is the default base path

--- src/utility/getBasePath.js
+++ src/utility/getBasePath.js
@@ -4,8 +4,13 @@ export default function getBasePath() {
 	if (configuration.basePath) {
 		return configuration.basePath
 	}
-	// `gh-pages` will have `/captchan` base path.
-	if (window.location.origin === 'https://catamphetamine.github.io') {
-		return window.location.pathname.slice(0, window.location.pathname.indexOf('/', '/'.length))
-	}
+	if (typeof window !== 'undefined') {
+		// // `gh-pages` will have `/captchan` base path.
+		// if (window.location.origin === 'https://catamphetamine.github.io') {
+		// 	return window.location.pathname.slice(0, window.location.pathname.indexOf('/', '/'.length))
+		// }
+		// `/captchan` is the conventional base path that can be autodetected.
+		if (window.location.pathname.indexOf('/captchan') === 0) {
+			return '/captchan'
+		}
 }
