0e7f3d0e0c9a8246deaa562efffe29b9a82e9ff1
catamphetamine <purecatamphetamine@gmail.com>
2019-09-15 23:25:08 +0300

    .
    Former-commit-id: 0bffd80c2b09be62289d7a760ec5ce35e3fdcf1d

---
+++ react-syntax-highlighter_languages_refractor_brainfuck.41503421d6c81ffa8666.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{1449:function(a,e,n){"use strict";function t(a){a.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}a.exports=t,t.displayName="brainfuck",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.41503421d6c81ffa8666.js.map
---
+++ react-syntax-highlighter_languages_refractor_hsts.41503421d6c81ffa8666.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[55],{1482:function(a,s,e){"use strict";function n(a){a.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}a.exports=n,n.displayName="hsts",n.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.41503421d6c81ffa8666.js.map
