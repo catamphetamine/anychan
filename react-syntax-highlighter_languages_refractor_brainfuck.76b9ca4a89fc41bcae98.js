527e165d380983523923012b1e49da62156a34b2
catamphetamine <purecatamphetamine@gmail.com>
2019-09-20 16:33:35 +0300

    .
    Former-commit-id: 5e48b3f30f883b5f47e871b4c5c5461e60027042

---
+++ react-syntax-highlighter_languages_refractor_brainfuck.76b9ca4a89fc41bcae98.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{1463:function(a,e,n){"use strict";function t(a){a.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}a.exports=t,t.displayName="brainfuck",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.76b9ca4a89fc41bcae98.js.map
