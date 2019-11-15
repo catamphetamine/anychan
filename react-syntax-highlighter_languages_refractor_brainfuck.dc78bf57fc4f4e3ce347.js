5e727df79f13e746804c43bb079a1526c8625f8a
catamphetamine <purecatamphetamine@gmail.com>
2019-11-16 01:39:21 +0300

    .
    Former-commit-id: 584e2984b040e95596d0237aede35778ecc5231e

---
+++ react-syntax-highlighter_languages_refractor_brainfuck.dc78bf57fc4f4e3ce347.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{1500:function(a,e,n){"use strict";function t(a){a.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}a.exports=t,t.displayName="brainfuck",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.dc78bf57fc4f4e3ce347.js.map
---
+++ react-syntax-highlighter_languages_refractor_git.dc78bf57fc4f4e3ce347.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[45],{1523:function(m,e,t){"use strict";function i(m){m.languages.git={comment:/^#.*/m,deleted:/^[-–].*/m,inserted:/^\+.*/m,string:/("|')(?:\\.|(?!\1)[^\\\r\n])*\1/m,command:{pattern:/^.*\$ git .*$/m,inside:{parameter:/\s--?\w+/m}},coord:/^@@.*@@$/m,commit_sha1:/^commit \w{40}$/m}}m.exports=i,i.displayName="git",i.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_git.dc78bf57fc4f4e3ce347.js.map
