264e9854ea078d4a133a515b18835d15069a8838
catamphetamine <purecatamphetamine@gmail.com>
2019-12-08 11:43:17 +0300

    'Accept': 'application/json'

--- src/api/Chan.js
+++ src/api/Chan.js
@@ -25,6 +25,9 @@ export default function Chan_({
 			request: (method, url, data) => {
 				return http[method.toLowerCase()](getProxyUrl(url), data, {
 					headers: {
+						// Sometimes imageboards may go offline while still responding with a web page:
+						// an incorrect 2xx HTTP status code with HTML content like "We're temporarily offline".
+						// Accepting only `application/json` HTTP responses works around that.
 						'Accept': 'application/json'
 					}
 				})
