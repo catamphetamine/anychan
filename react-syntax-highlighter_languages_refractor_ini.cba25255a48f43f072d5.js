52f6d367c4baee463fc5e05ee2447b83ab7c926d
catamphetamine <purecatamphetamine@gmail.com>
2019-12-19 20:04:20 +0300

    .

---
+++ react-syntax-highlighter_languages_refractor_ini.cba25255a48f43f072d5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[60],{1831:function(t,n,i){"use strict";function s(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=s,s.displayName="ini",s.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.cba25255a48f43f072d5.js.map
