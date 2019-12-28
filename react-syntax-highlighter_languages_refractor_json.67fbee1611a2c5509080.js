11d1e3956154aef5f8ea037212b9cdb98442742e
catamphetamine <purecatamphetamine@gmail.com>
2019-12-28 21:37:09 +0300

    .

---
+++ react-syntax-highlighter_languages_refractor_json.67fbee1611a2c5509080.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[66],{1848:function(n,e,s){"use strict";function t(n){n.languages.json={comment:/\/\/.*|\/\*[\s\S]*?(?:\*\/|$)/,property:{pattern:/"(?:\\.|[^\\"\r\n])*"(?=\s*:)/,greedy:!0},string:{pattern:/"(?:\\.|[^\\"\r\n])*"(?!\s*:)/,greedy:!0},number:/-?\d+\.?\d*(e[+-]?\d+)?/i,punctuation:/[{}[\],]/,operator:/:/,boolean:/\b(?:true|false)\b/,null:{pattern:/\bnull\b/,alias:"keyword"}}}n.exports=t,t.displayName="json",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_json.67fbee1611a2c5509080.js.map
