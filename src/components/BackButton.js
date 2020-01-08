2f78c9ae9d6281e3b1ee4521387378608d49f601
catamphetamine <purecatamphetamine@gmail.com>
2020-01-08 08:54:05 +0300

    Board/Thread page paddings/margins fix

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -13,15 +13,19 @@ import './BackButton.css'
 export default function BackButton() {
 	const locale = useSelector(({ settings }) => settings.settings.locale)
 	const [canGoBack, goBack] = useCanGoBackFromThreadToBoard()
-	if (!canGoBack) {
-		return null
-	}
+	// Not rendering the button on some pages resulted in different layout
+	// on different pages, that wouldn't be optimal in terms of UX.
+	// if (!canGoBack) {
+	// 	return null
+	// }
 	return (
 		<button
 			type="button"
 			title={getMessages(locale).actions.back}
 			onClick={goBack}
-			className="rrui__button-reset BackButton">
+			className={classNames('rrui__button-reset', 'BackButton', {
+				'BackButton--hidden': !canGoBack
+			})}>
 			<LeftArrow className="BackButtonIcon"/>
 		</button>
 	)
