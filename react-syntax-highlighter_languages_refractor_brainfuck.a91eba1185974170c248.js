33622c3d6cf542347785dd809c9434c9cac41483
catamphetamine <purecatamphetamine@gmail.com>
2020-04-20 04:37:54 +0300

    .

---
+++ react-syntax-highlighter_languages_refractor_brainfuck.a91eba1185974170c248.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{1582:function(a,e,n){"use strict";function t(a){a.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}a.exports=t,t.displayName="brainfuck",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.a91eba1185974170c248.js.map
